c50d8326b4556e7885d6cf87691715d6a5e7b438
harpreet2007 <2003harpreetkaur02@gmail.com>
2024-08-22 11:42:08 -0600

    my commit

---
+++ controllers/authController.js
@@ -0,0 +1,5 @@
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+const AttendanceManager = require('../models/attendanceMaanager.js');
+require('dotenv').config();
+
---
+++ models/attendanceManager.js
@@ -0,0 +1,24 @@
+const mongoose = require('mongoose');
+//Defining the schema for attendanceManager
+const attendanceManagerSchema = new mongoose.Schema({
+    email:{
+        type: String,
+        required: true,
+        unique: true,
+        trim: true,
+
+    },
+    password:{
+        type: String,
+        required: true,
+
+    }, 
+    role: {
+        type: String,
+        default: 'Attendance Manager',
+        enum: ['attendance-manager', 'Admin'],
+    },
+    timestamps: true,
+});
+const AttendanceManager = mongoose.model('AttendanceManager', attendanceManagerSchema);
+module.exports = AttendanceManager;
